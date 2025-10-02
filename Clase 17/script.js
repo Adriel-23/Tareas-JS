c1465ebff94134eb093d682f4892429ba04570a1
Adriel23 <adrieldev23@gmail.com>
2025-10-02 00:54:36 -0300

    Clase 17

---
+++ Clase 17/script.js
@@ -0,0 +1,61 @@
+/* Bucles */
+
+/* let numero = prompt ("ingrese un numerito")
+
+if (!numero || isNaN(numero)) {
+    alert ("El número no es válido")
+    numero = prompt ("ingrese un numerito nuevamente")
+    if (!numero || isNaN(numero)) {
+        alert ("sos boludo?")
+    }
+    else {
+        alert ("Bien mi gente latino")
+    }
+} */
+
+/*   let numero = prompt ("Ingrese un númerito")
+    
+    while (!numero || isNaN(numero)) {
+        alert ("Error: el número no es válido")
+        numero = prompt ("ingrese un númerito nuevamente") 
+        
+    }
+    alert ("Perfecto señor, eso si es un númerito") */
+
+
+    /* Solicitar contraseña */
+
+    /* let password_db = ("Freya")
+
+    let password_db_solicitud = prompt ("Ingrese la contraseña")
+
+    while (password_db_solicitud != password_db || password_db_solicitud !== password_db){
+        if (password_db_solicitud === null){
+            alert ("Cancelaste la operación"); 
+            break;
+        }
+        alert ("Contraseña incorrecta")
+            password_db_solicitud = prompt ("Ingrese nuevamente la contraseña")
+    }
+    if (password_db_solicitud === password_db){
+    alert ("Contraseña correcta, acceso permitido")
+    } */
+
+
+    /* 
+    -Solicitar una operación 
+    -Validar que dicha operación sea "Iva" o "Descuento"
+        -Si no es, volver a solicitar, a excepción de que el usuario de cancelar, en dicho caso, cancelar el programa y avisar al usuario.
+        - Válidar el número, en caso de no ser correcto volver a solicitar. (EL número debe ser positivo)
+        - Mostrar por alerta el 21% del precio ingresado.
+    -Si la operación es: "Descuento" Solicitar un número.
+        - Válidar el número, en caso de no ser correcto volver a solicitar (El núimero debe ser positivo)
+        - Solicitar número de descuento.
+            - Válidar el número de descuento (El número debe ser positivo y menor que 100)
+                - Volver a solicitar en caso de ser incorrecto.
+                - Mostrar por alerta el porcentaje del número ingresado. Ej:  "El precio con descuento aplicado es de ${Resultado}"
+    *
+
+
+
+    
